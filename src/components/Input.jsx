45d326fde5cb9bc91509194aed9f30079df68ce0
Khantuev Andrei <khantuevandrei@gmail.com>
2025-11-02 10:04:51 +0500

    Add onChange handler to Input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,8 @@
-export default function Input({id, labelName, type, value}) {
+export default function Input({id, labelName, type, value, onChange}) {
   return (
     <>
       <label htmlFor={id}>{labelName}</label>
-      <input id={id} type={type} value={value} />
+      <input id={id} type={type} value={value} onChange={onChange}/>
     </>
-    
   )
 }
